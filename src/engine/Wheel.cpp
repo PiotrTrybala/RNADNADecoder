c6ab7feb854f031bbe13900eab6de4bcd782c513
Piotr Trybała <trybalapiotr4@gmail.com>
2022-11-30 16:05:43 +0100

    [Wheel] starting impl of layers creation

--- src/engine/Wheel.cpp
+++ src/engine/Wheel.cpp
@@ -31,7 +31,31 @@ namespace decoder
 
             json wheel = json::parse(*this->wheelRep);
 
-            std::cout << "Layers amount: " << wheel["layersAmount"] << "\n";
+
+            this->layersAmount = wheel["layersAmount"].get<int>();
+
+            auto layers = wheel["layers"];
+            int level = 1;
+
+            std::vector<std::vector<std::string> > jsonLayers;
+
+            for (auto& layer : layers) {
+                std::vector<std::string> values = layer["values"].get<vector<std::string> >();
+                jsonLayers.push_back(values);
+            }
+
+
+
+            // auto results = wheel["results"];
+            // int resultIndex = 0;
+            // for (auto& result : results) {
+            //     std::cout << "Result #" << resultIndex++ << ": \n";
+            //     std::cout << "Name: " << result["name"].get<std::string>() << "\n";
+            //     std::cout << "Symbol: " << result["symbol"].get<std::string>() << "\n";
+            //     std::cout << "Parents: " << result["parents"].get<int>() << "\n";
+            // }
+
+
         }
     }
 };
