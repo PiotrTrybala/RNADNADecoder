0a118b0a1e1a1a88f185b4647c58e1b410d9365c
Piotr Trybała <trybalapiotr4@gmail.com>
2022-12-01 11:15:44 +0100

    [Decoder] defined CompoundBase header class

--- src/engine/EngineTypes.hpp
+++ src/engine/EngineTypes.hpp
@@ -2,19 +2,26 @@
 #include <cstring>
 #include <unordered_map>
 
-namespace decoder {
-    namespace types {
+namespace decoder
+{
+    namespace engine
+    {
+        namespace types
+        {
 
-        struct compound_schema {
-            std::string const_part;
-            std::vector<std::string> variadic_part;
-        };
+            struct compound_schema
+            {
+                std::string const_part;
+                std::vector<std::string> variadic_part;
+            };
 
-        struct compound {
-            std::string name;
-            std::string symbol;
-            struct compound_schema schema;
-        };
+            struct compound
+            {
+                std::string name;
+                std::string symbol;
+                struct compound_schema schema;
+            };
 
+        }
     }
 }
