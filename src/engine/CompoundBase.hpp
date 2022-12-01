b080f1902dba7ca776f160d159bf1a87e73a1cc7
Piotr Trybała <trybalapiotr4@gmail.com>
2022-12-01 11:08:58 +0100

    [Decoder] defined engine types

---
+++ src/engine/CompoundBase.hpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include "nlohmann/json.hpp"
+
+using nlohmann::json;
+
+namespace decoder {
+
+    class CompoundBase {
+        private:
+
+
+
+        public:
+    };
+
+
+}
---
+++ src/engine/EngineTypes.hpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <cstring>
+#include <unordered_map>
+
+namespace decoder {
+    namespace types {
+
+        struct compound_schema {
+            std::string const_part;
+            std::vector<std::string> variadic_part;
+        };
+
+        struct compound {
+            std::string name;
+            std::string symbol;
+            struct compound_schema schema;
+        };
+
+    }
+}
