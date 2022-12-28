8e2b2abc378312c9b1b73778cd126d7b6b6acf67
Piotr Trybała <trybalapiotr4@gmail.com>
2022-12-28 11:49:12 +0100

    [HttpParser] added documentation

--- src/api/include/HttpParser.hpp
+++ src/api/include/HttpParser.hpp
@@ -9,7 +9,19 @@ namespace decoder {
     namespace http {
         class Parser {
             public:
+                /**
+                * @brief Parses raw HTTP request to struct http_request
+                *
+                * @param req raw HTTP request
+                * @return struct http_request struct holding parsed infomations
+                */
                 static struct http_request ParseRequest(std::string& req);
+                /**
+                 * @brief Transforms struct http_response to raw HTTP response ready to be sent to receiver
+                 *
+                 * @param res struct holding informations
+                 * @return std::string raw HTTP response
+                 */
                 static std::string PrepareResponse(struct http_response& res);
         };
 
