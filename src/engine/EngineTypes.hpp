75ba8982d681866e4950532e8c2f4722ddca4ee9
Piotr Trybała <trybalapiotr4@gmail.com>
2022-12-28 11:39:43 +0100

    [EngineTypes] added comments to types

--- src/engine/EngineTypes.hpp
+++ src/engine/EngineTypes.hpp
@@ -8,12 +8,26 @@ namespace decoder
 {
     namespace engine
     {
+        /**
+         * @brief struct compound_schema unparsed representation of compound
+         *
+         * @param const_part part in compound which is not changing in group of compounds
+         * @param variadic_part part of compound which is different depending of compound in group
+         *
+         */
         struct compound_schema
         {
             std::string const_part;
             std::vector<std::string> variadic_part;
         };
 
+        /**
+         * @brief struct compound parsed representation
+         *
+         * @param name name of parsed compound (Syr, ...)
+         * @param symbol symbol of parsed compound (K, L, M, ...)
+         * @param schema group of 3 letters which identifies the compound
+         */
         struct compound
         {
             std::string name;
