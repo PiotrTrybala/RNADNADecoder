146cc67aec6bfb7b08acf33669e41fbb115b2798
Piotr Trybała <trybalapiotr4@gmail.com>
2022-12-28 10:48:29 +0100

    [DecoderEngine] fixed generation of outputs in GetResults

--- src/engine/DecoderEngine.cpp
+++ src/engine/DecoderEngine.cpp
@@ -18,24 +18,26 @@ namespace decoder
             delete base;
         }
 
-        auto IsRightMultiple(int input) -> bool {
-            int multiple_length = (input / 3) * 3;
-            return input != multiple_length;
-        }
-
         auto DecoderEngine::GetResults(const std::string& input) -> std::vector<std::string> {
             std::vector<std::string> results;
-            bool extend = IsRightMultiple(input.length());
-            int total_length {input.length() / 3 * 3};
+            bool extend = false;
+            int input_length = input.length();
+            std::cout << input_length << " | il" << std::endl;
+            int total_length {(input_length / 3) * 3};
+            std::cout << total_length << " | tl" << std::endl;
+            if (input_length != total_length) extend = true;
             for (int i = 0; i < 3; i++) {
-                results.push_back(GetPatrialResult(std::move(input)));
+                std::string substr_input {input};
+                results.push_back(GetPatrialResult(std::move(substr_input.substr(i, total_length))));
                 if (extend) total_length++;
             }
             return results;
         }
         auto DecoderEngine::GetPatrialResult(const std::string input) -> std::string {
             std::string output{""}, raw_input {input};
+            #ifdef DEBUG_DECODERENGINE_GETPARTIALRESULT
             spdlog::get("console")->info("Decoding sequence: {}", raw_input);
+            #endif
             for (int i = 0; i < raw_input.length(); i += 3) {
                 std::string seq { raw_input.substr(i, 3) };
 
@@ -45,6 +47,7 @@ namespace decoder
 
                 output += base->GetSymbol(seq);
             }
+            std::cout << "partial result: " << output << std::endl;
             return output;
         }
     }
