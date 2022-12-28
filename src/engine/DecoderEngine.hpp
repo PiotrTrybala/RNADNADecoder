540b4eabbc7d1920e073651ccb4a4d4c55a46e69
Piotr Trybała <trybalapiotr4@gmail.com>
2022-12-28 11:43:31 +0100

    [DecoderEngine] added documentation

--- src/engine/DecoderEngine.hpp
+++ src/engine/DecoderEngine.hpp
@@ -15,7 +15,20 @@ namespace decoder {
                 DecoderEngine();
                 ~DecoderEngine();
 
+                /**
+                 * @brief Parses input which is chain of compounds and outputs resulting chain of amino acids
+                 *
+                 * @param input chain of raw compounds
+                 *
+                 * @return std::vector<std::string> chains of amino acids decoded from inputs
+                 */
                 std::vector<std::string> GetResults(const std::string& input);
+                /**
+                 * @brief Helper method for GetResults for decoding single version of chain
+                 *
+                 * @param input chain of raw compoundss
+                 * @return std::string decoded chain
+                 */
                 std::string GetPatrialResult(const std::string input);
         };
     }
