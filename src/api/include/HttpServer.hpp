60517c202affc48a082e9291d5f9c71b1f540e42
Piotr Trybała <trybalapiotr4@gmail.com>
2022-12-28 11:51:41 +0100

    [HttpServer] added documentation

--- src/api/include/HttpServer.hpp
+++ src/api/include/HttpServer.hpp
@@ -19,9 +19,25 @@ namespace decoder {
                 tcp::acceptor server_acceptor;
                 std::vector<struct endpoint_reg> registry;
                 using pointer = boost::shared_ptr<TcpConnection>;
-
+                /**
+                 * @brief
+                 *
+                 * @param method
+                 * @param endpoint
+                 * @param func
+                 */
                 void MakeRegistryEntry(request_method method, std::string endpoint, endpoint_function& func);
+                /**
+                 * @brief
+                 *
+                 * @param new_connection
+                 * @param error
+                 */
                 void HandleConnection(pointer new_connection, const boost::system::error_code& error);
+                /**
+                 * @brief
+                 *
+                 */
                 void StartAccept();
             public:
 
@@ -30,13 +46,22 @@ namespace decoder {
                 HttpServer(const HttpServer& rhs);
                 HttpServer& operator=(const HttpServer& rhs);
                 ~HttpServer();
-
+                /**
+                 * @brief Get, Post, Put, Del registers endpoint on the server
+                 *
+                 * @param endpoint path to given endpoint
+                 * @param func callback method to be executed when visiting given endpoint
+                 */
                 void Get(std::string endpoint, endpoint_function func);
                 void Post(std::string endpoint, endpoint_function func);
                 void Put(std::string endpoint, endpoint_function func);
                 void Del(std::string endpoint, endpoint_function func);
-
-                void Redirect(std::string redirect_endpoint);
+                /**
+                 * @brief Redirects users to specified endpoint
+                 *
+                 * @param redirect_endpoint specified location
+                 */
+                struct http_response Redirect(struct http_response res, std::string redirect_endpoint);
                 void Run();
                 void Stop();
 
