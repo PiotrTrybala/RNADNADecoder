cfcd1701e92b49d890c1b4196feafb770e1aa3c3
Piotr Trybała <trybalapiotr4@gmail.com>
2022-12-28 11:46:49 +0100

    [CompoundBase] added documentation

--- src/engine/CompoundBase.hpp
+++ src/engine/CompoundBase.hpp
@@ -25,14 +25,34 @@ namespace decoder
             std::ifstream* compound_schema_stream;
             std::unordered_map<std::string, struct compound> compound_symbol_lookup;
 
-            const struct compund& GetCompoundBySequence(const std::string& seq);
+            /**
+             * @brief Parses raw schema into struct compound_schema
+             *
+             * @param raw_schema raw schema from file
+             * @return struct compound_schema parsed schema into const part and variadic part
+             */
             struct compound_schema MakeCompoundSchema(const std::string&& raw_schema);
+
+            /**
+             * @brief Parses schema file in JSON format
+             *
+             */
             void ParseSchema();
 
         public:
-
+            /**
+             * @brief Construct a new Compound Base object
+             *
+             * @param filepath path to JSON file storing schema
+             */
             CompoundBase(const std::string& filepath);
             ~CompoundBase();
+            /**
+             * @brief Gets symbol for specified compound sequence
+             *
+             * @param seq compound
+             * @return std::string symbol related to given compound
+             */
             std::string GetSymbol(const std::string& seq);
 
         };
